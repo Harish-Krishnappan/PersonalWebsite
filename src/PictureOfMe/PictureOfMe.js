38501617dfd1f0d42bf977fd207311d38a4f6ac9
[Harish Krishnappan] <[hkrishnappan2@huskers.unl.edu]>
2023-03-19 16:26:22 -0500

    made frontPic polish

--- src/PictureOfMe/PictureOfMe.js
+++ src/PictureOfMe/PictureOfMe.js
@@ -6,7 +6,7 @@ function PictureOfMe() {
     <section id = "me">
     <div className="container">
       <div className="row justify-content-center align-items-center">
-        <div className="col-md-6 col-lg-4">
+        <div className="col-md-6">
           <div className="PictureOfMe overflow-hidden">
             <img src={nbImage} alt="logo" className="w-100 h-100 object-fit-cover"/>
           </div>
